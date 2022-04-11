cda63e58e823e763e49edef0f328a50883d0b2b1
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-11 16:41:05 +0100

    fixed test for bookmark

--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -2,7 +2,7 @@ require 'bookmark'
 describe Bookmark do
   describe '#show the bookmarks' do
     it 'displays the book marks to the users' do
-      expect { subject.all }.not_to raise_error
+      expect(described_class.all).not_to eq []
     end
   end
 end

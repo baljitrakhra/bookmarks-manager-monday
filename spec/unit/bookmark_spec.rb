4e146f29c41b77ac6e37ab8789edbc35a4191005
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-13 08:27:13 +0100

    unit test updated and now they access test data'

--- spec/unit/bookmark_spec.rb
+++ spec/unit/bookmark_spec.rb
@@ -1,8 +1,16 @@
 require 'bookmark'
+require 'pg'
+
 describe Bookmark do
   describe '#show the bookmarks' do
     it 'displays the book marks to the users' do
+     connection = PG.connect(dbname: 'bookmark_manager_test')
+     connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com/');")
+     connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+     connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com/');")
+     
      bookmarks = Bookmark.all
+     
      expect(bookmarks).to include "http://www.makersacademy.com/"
      expect(bookmarks).to include "http://www.destroyallsoftware.com"
      expect(bookmarks).to include "http://www.google.com/"

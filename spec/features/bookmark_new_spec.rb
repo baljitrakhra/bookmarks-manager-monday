04ef50bb08488120b7f233d1e2a6851751589fd8
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-13 15:41:55 +0100

    Adding titles to new bookmarks feature test and views implemented

--- spec/features/bookmark_new_spec.rb
+++ spec/features/bookmark_new_spec.rb
@@ -2,7 +2,8 @@ feature 'form' do
   scenario' page to have text area to add your bookmark' do
     visit('/form') 
     fill_in 'bname', with: 'http://amazon.co.us'
+    fill_in 'title', with: 'Amazon'
     click_button 'Submit'
-    expect(page).to have_content 'http://amazon.co.us'
+    expect(page).to have_content 'Amazon'
   end
 end  
--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -2,7 +2,8 @@ feature 'form' do
   scenario' page to have text area to add your bookmark' do
     visit('/form') 
     fill_in 'bname', with: 'http://amazon.co.us'
-    expect(page).to have_content 'Add new book mark here'
+    fill_in 'title', with: 'Amazon'
+    expect(page).to have_content 'Add new bookmark url and title here'
     expect(page).to have_button 'Submit'
   end
 end

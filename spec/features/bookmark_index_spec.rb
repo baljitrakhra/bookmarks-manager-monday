69f6ae0c6f4ef1a1949aa792b3d7fa3f047f4ef7
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-13 15:11:24 +0100

    refactored test

--- spec/features/bookmark_index_spec.rb
+++ spec/features/bookmark_index_spec.rb
@@ -5,12 +5,10 @@ feature 'bookmark' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
     # Add the test data
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
-
+    Bookmark.add_bookmark('http://www.makersacademy.com')
+    Bookmark.add_bookmark('http://www.destroyallsoftware.com')
+    Bookmark.add_bookmark('http://www.google.com')
     visit('/bookmarks')
-
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.destroyallsoftware.com"
     expect(page).to have_content "http://www.google.com"

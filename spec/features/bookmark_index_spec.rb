7051c182b90eaa6b7cea8197fbb88cafb372147f
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-14 16:42:12 +0100

    WIP for updating the bookmark

--- spec/features/bookmark_index_spec.rb
+++ spec/features/bookmark_index_spec.rb
@@ -4,13 +4,13 @@ feature 'bookmark' do
   scenario 'user can view the list of bookmarks' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    # Add the test data
+    
     Bookmark.add_bookmark(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     Bookmark.add_bookmark(url: 'http://www.destroyallsoftware.com', title: 'Destroy all')
-    # Bookmark.add_bookmark(url: 'http://www.google.com', title: 'Google for all')
+    
     visit('/bookmarks')
     expect(page).to have_content 'Makers Academy'
-    # expect(page).to have_link('Destroy all', href: 'http://www.destroyallsoftware.com')
+    
   end
 
   scenario 'page will have a button to add a new bookmark' do
---
+++ spec/features/updating_a_bookmark_spec.rb
@@ -0,0 +1,17 @@
+feature 'updating a bookmark' do
+  scenario 'A user can update a bookmark' do
+    bookmark = Bookmark.add_bookmark(url: 'http://google.com/', title: 'Google homepage')
+    visit('/bookmarks')
+    expect(page).to have_content('Google homepage')
+
+    first('.bookmark').click_button('Edit')
+    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
+    
+    fill_in('url', with: "http://poople.com")
+    fill_in('title', with: "Poople homepage")
+    click_button('Submit')
+
+    expect(current_path).to eq '/bookmarks'
+    expect(page).to have_link("Poople homepage", href: 'http://poople.com')
+  end
+end
